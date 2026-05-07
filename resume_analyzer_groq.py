5a3a2f40a422c8908ca9ef8b2d1e991a575e7f0d
bachir <Mehdixbachir@gmail.com>
2026-05-07 18:52:01 +0100

    push

--- resume_analyzer_groq.py
+++ resume_analyzer_groq.py
@@ -320,6 +320,7 @@ def main():
                 ask_button = st.button("Get Answer 🔍", key="question_btn",
                                        disabled=not (api_key and question))
 
+ 
             if ask_button:
                 st.markdown(section_header("💬 Answer", "✨"), unsafe_allow_html=True)
                 answer_placeholder = st.empty()
